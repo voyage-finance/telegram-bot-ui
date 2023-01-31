570da1d1952e0e66f69c8f1187647a8431948a45
daulet <daulet.issatayev@nu.edu.kz>
2023-01-31 13:27:25 +0600

    fix: lint errors

--- pages/safes/[safeAddress]/transactions/create/index.tsx
+++ pages/safes/[safeAddress]/transactions/create/index.tsx
@@ -1,24 +1,8 @@
 import { Button, Text } from "@components/atoms";
 import Card from "@components/moleculas/Card";
-import ConnectBtn from "@components/moleculas/WalletNavItem/ConnectBtn";
-import { Group, Stack, TextInput } from "@mantine/core";
-import { useForm } from "@mantine/form";
-import {
-  OperationType,
-  SafeTransactionDataPartial,
-} from "@safe-global/safe-core-sdk-types";
-import { fetchSigner } from "@wagmi/core";
-import { ethers } from "ethers";
+import { Stack } from "@mantine/core";
 import Head from "next/head";
-import { useEffect, useState } from "react";
-import { SafeService } from "services/safeSdk";
-import { useSigner } from "wagmi";
-import BigNumber from "bignumber.js";
-import styles from "./styles.module.scss";
-import TitleWithLine from "@components/moleculas/TitleWithLine";
-import { formatAmount } from "@utils/bn";
 import { useRouter } from "next/router";
-import { showNotification } from "@mantine/notifications";
 
 const txBuildUrl =
   "https://app.safe.global/matic:0x775AD9C18e0D8dE7dFCFfc8540a0203F61b39A7e/apps?appUrl=https%3A%2F%2Fapps.gnosis-safe.io%2Ftx-builder";
--- src/hooks/index.tsx
+++
@@ -1,1 +0,0 @@
-export { useIsWrongNetwork } from "./useIsWrongNetwork";
